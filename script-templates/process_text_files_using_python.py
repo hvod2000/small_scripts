c823fc63695cff6f3ea73b7ca2cf32a9d0da2d60
Uladzislau Khamkou <kham@tuta.io>
2023-10-05 23:38:13 +0300

    Update script template

--- script-templates/process_text_files_using_python.py
+++ script-templates/process_text_files_using_python.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Aboba about aboba.
+SCRIPT_DESCRIPTION
 
 Usage: {script} [options] [PATH]
 
@@ -16,42 +16,46 @@ Options:
         Raise verbosity level.
 
     -z, --zero-terminated
-        Output is terminated by NUL rather than EOL.
+        Outputed items are terminated by NUL rather than EOL.
 """
 import sys
 from contextlib import suppress
 from pathlib import Path
 
 
-def process(file: Path):
-    lines = list(file.read_text().removesuffix("\n").split("\n"))
+def process(text: str):
+    lines = list(text.removesuffix("\n").split("\n"))
     for line in lines:
-        println(line)
+        emit(line)
 
 
 def main(script_name: str, *script_args: str):
     doc = __doc__.format(script=Path(script_name).name)
     args = __import__("docopt").docopt(doc, script_args)
     setattr(debug, "enabled", args["--verbose"])
-    setattr(println, "eol", "\x00" if args["--zero-terminated"] else "\n")
+    setattr(emit, "eol", "\x00" if args["--zero-terminated"] else "\n")
     debug(f"Running with arguments {dict(args)!r}")
     paths = [Path(args["PATH"] or ".")]
     for path in paths:
         if path.is_dir():
             paths.extend(path.joinpath(f) for f in path.iterdir())
         elif path.is_file():
-            process(path)
+            debug(f"Processing {path}")
+            try:
+                process(path.read_text(encoding="utf-8", errors="strict"))
+            except UnicodeDecodeError:
+                debug(f"  Failure: {path} is binary file")
 
 
-def println(line: object):
-    print(str(line), end=getattr(println, "eol", "\n"))
+def emit(*objects: object, sep: str = " "):
+    print(sep.join(map(str, objects)), end=getattr(emit, "eol", "\n"))
 
 
-def debug(msg: str):
+def debug(*objects: object, sep: str = " "):
     if getattr(debug, "enabled", False):
-        print(msg)
+        print(sep.join(map(str, objects)))
 
 
 if __name__ == "__main__":
     with suppress(KeyboardInterrupt):
-        main(*sys.argv)
+        main(sys.argv[0], *sys.argv[1:])
