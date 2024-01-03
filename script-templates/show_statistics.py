efe15d5d3e9e46c5f4545d1c0f957448c7e7d600
Uladzislau Khamkou <kham@tuta.io>
2024-01-03 23:07:01 +0300

    Add new script template

---
+++ script-templates/show_statistics.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""
+SCRIPT_DESCRIPTION
+
+Usage: {script} [options]
+
+Options:
+    -h, --help
+        Show this screen and exit.
+"""
+import sys
+from contextlib import suppress
+from pathlib import Path
+
+
+def main(script_name: str, *script_args: str):
+    doc = __doc__.format(script=Path(script_name).name)
+    if script_args:
+        print(doc.lstrip())
+        if len(script_args) != 1 or script_args[0] not in ["-h", "--help"]:
+            exit(1)
+
+    pass
+
+
+if __name__ == "__main__":
+    with suppress(KeyboardInterrupt):
+        main(sys.argv[0], *sys.argv[1:])
